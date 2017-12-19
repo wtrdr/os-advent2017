fc5c4ec7c4b7d0c8cbff157f328cb854a583822b
yasuyuki toriumi <toriumi0118@gmail.com>
2017-12-19 22:08:25 +0900

    add: os

--- 06/bootload/main.c
+++ 06/bootload/main.c
@@ -48,6 +48,8 @@ int main(void)
   static char buf[16];
   static long size = -1;
   static unsigned char *loadbuf = NULL;
+  char *entry_point;
+  void (*f)(void);
   extern int buffer_start;
 
   init();
@@ -72,7 +74,16 @@ int main(void)
       puts("\n");
       dump(loadbuf, size);
     } else if (!strcmp(buf, "run")) {
-      elf_load(loadbuf);
+      entry_point = elf_load(loadbuf);
+      if (!entry_point) {
+        puts("run error!\n");
+      } else {
+        puts("starting from entry point: ");
+        putxval((unsigned long)entry_point, 0);
+        puts("\n");
+        f = (void (*)(void))entry_point;
+        f();
+      }
     } else {
       puts("unknown.\n");
     }
