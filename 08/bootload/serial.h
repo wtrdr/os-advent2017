b1f0ff0b67dfa3d4ac34add40df3ce3873afb9a2
yasuyuki toriumi <toriumi0118@gmail.com>
2018-01-13 10:37:39 +0900

    Fix and Add

--- 08/bootload/serial.h
+++ 08/bootload/serial.h
@@ -6,11 +6,5 @@ int serial_is_send_enable(int index);             // 送信可能？
 int serial_send_byte(int index, unsigned char b); // 1文字送信
 int serial_is_recv_enable(int index);
 unsigned char serial_recv_byte(int index);
-int serial_intr_is_send_enable(int index);
-void serial_intr_send_enable(int index);
-void serial_intr_send_disable(int index);
-int serial_intr_is_recv_enable(int index);
-void serial_intr_recv_enable(int index);
-void serial_intr_recv_disable(int index);
 
 # endif
